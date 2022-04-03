2ac03c4a1ab1f5fdb860153a4e7b8c278cb9901e
rienath <infernalion@hotmail.com>
2022-04-03 15:20:56 +0100

    Further server configs

--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from "mongoose";
 
-const app = express();
+// Environmental variables
 dotenv.config();
 
-// Increase the request body size as we will send images
-app.use(bodyParser.json({ limit: '10mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
-// Enable Cross-Origin Resource Sharing
+// Server configs
+const app = express();
+app.use(express.json({ limit: '10mb'}))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(cors());
 
-// 5000 is used on macs
-const PORT = process.env.PORT || 6000;
+// Routes
+
 
 // Connect to database
+const PORT = process.env.PORT || 6000; // Port 5000 already used in macs
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => app.listen(PORT, () => console.log(`Server up: http://localhost:${PORT}`)))
     .catch((error) => console.log(`Server down: ${error}`));
