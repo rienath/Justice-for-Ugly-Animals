1af909b95bf8a42a3eb7e16069af3ee2e984d6fc
rienath <infernalion@hotmail.com>
2022-04-04 20:07:45 +0100

    Minor fixes

--- client/src/constants/actionTypes.js
+++
@@ -1,6 +0,0 @@
-export const LANDING_TEXT = `We are a society which tries to raise the profile of the <b>ugly</b> animals
-            of the world. We believe that fluffy, cute creatures of the world,
-            such as the giant panda, get far too much attention in the modern media.
-            Creatures like the naked mole rat, the spiny lumpsucker fish and the jumping spider deserve much
-            more attention! Of course, <b>ugly</b> is subjective and might apply to lots of animals, too,
-            but definitely not pandas, and blob fish are now considered passé, too.`
