4777c754a84fa90325905e31b1dc120083553564
rienath <infernalion@hotmail.com>
2022-04-06 13:08:26 +0100

    User authentication middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+// Middleware that checks if user has a token and gets the data out it
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        let decodedData;
+        decodedData = jwt.verify(token, process.env.JWT_SECRET);
+        req.userID = decodedData?._id;
+        req.username = decodedData?.username
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default auth;
